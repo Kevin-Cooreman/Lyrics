1f2e156f36c787fda8f237d0cc290a6d299750ff
FLORIAN <florian.delplancke@student.kuleuven.be>
2023-04-20 16:46:16 +0200

    project en projects aangemaakt

---
+++ app/src/main/java/com/example/lyrics/ProjectsListActivity.java
@@ -0,0 +1,13 @@
+package com.example.lyrics;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projects_list);
+    }
+}
