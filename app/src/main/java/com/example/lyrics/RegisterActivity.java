d7d1e2b4fd1756d2c96edbbee8afbd6db26b2a3f
FLORIAN <florian.delplancke@student.kuleuven.be>
2023-04-27 16:57:38 +0200

    register werkt bna

--- app/src/main/java/com/example/lyrics/RegisterActivity.java
+++ app/src/main/java/com/example/lyrics/RegisterActivity.java
@@ -13,16 +13,19 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
 
 
-    String REGISTER_URL = "https://studev.groept.be/api/a22pt108/registerrequest/";
-    String CHECKUSERNAME_URL = "https://studev.groept.be/api/a22pt108/checkIfUsernameIsInUse/";
+    String REGISTER_URL = "https://studev.groept.be/api/a22pt108/registerrequest";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,72 +38,47 @@ public class RegisterActivity extends AppCompatActivity {
         String username = usernameEditText.getText().toString();
         EditText passwordEditText = findViewById(R.id.newPasswordInput);
         String password = passwordEditText.getText().toString();
-        checkIfUsernameIsInUse(username, password);
+        requestRegister(username, password);
     }
-
-    private void checkIfUsernameIsInUse(String username, String password) {
-        String MODIFIED_CHECKUSERNAME_URL = CHECKUSERNAME_URL + "/" + username;
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        JsonArrayRequest queueRequest = new JsonArrayRequest(
-                Request.Method.GET,
-                MODIFIED_CHECKUSERNAME_URL,
-                null,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        if( response.length() != 0){
-                            //user bestaat al
-                            TextView theView = (TextView) findViewById(R.id.tempTextRegister);
-                            String ErrorMessage = getString(R.string.usernameAlreadyExists);
-                            theView.setText(ErrorMessage);
-                            theView.setVisibility(View.VISIBLE);
-
-                        }
-                        else{
-                            requestRegister(username, password);
-                            TextView theView = (TextView) findViewById(R.id.tempTextRegister);
-                            String ErrorMessage = "username bestaat nog niet";
-                            theView.setText(ErrorMessage);
-                            theView.setVisibility(View.VISIBLE);
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                });
-        requestQueue.add(queueRequest);
-    }
-
 
     private void requestRegister(String username, String password) {
-        String MODIFIED_REGISTER_URL = REGISTER_URL + username + "/" + password;
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
-        JsonArrayRequest queueRequest = new JsonArrayRequest(
+        StringRequest submitRequest = new StringRequest(
                 Request.Method.POST,
-                MODIFIED_REGISTER_URL,
-                null,
-                new Response.Listener<JSONArray>() {
+                REGISTER_URL,
+                new Response.Listener<String>() {
                     @Override
-                    public void onResponse(JSONArray response) {
-                        successfulRegister();
+                    public void onResponse(String response) {
+                        if( response.length() != 0){
+                            successfulRegister();
+                        }
                         TextView theView = (TextView) findViewById(R.id.tempTextRegister);
-                        String ErrorMessage = "geregistreerd";
-                        theView.setText(ErrorMessage);
+                        theView.setText(response);
                         theView.setVisibility(View.VISIBLE);
+
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
+                        TextView theView = (TextView) findViewById(R.id.tempTextRegister);
+                        String loginErrorMessage = "Mislukt, is de username miss al in gebruik?";
+                        theView.setText(loginErrorMessage);
+                        theView.setVisibility(View.VISIBLE);
                     }
-                });
-        requestQueue.add(queueRequest);
+                }
+        ) { //NOTE THIS PART: here we are passing the POST parameters to the webservice
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        requestQueue.add(submitRequest);
     }
 
     public void successfulRegister(){
