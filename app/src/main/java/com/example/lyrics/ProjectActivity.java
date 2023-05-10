763434d19aa80072f95365d32c4eb7d1a5650d8d
kevin <kevin.cooreman@student.kuleuven.be>
2023-05-10 23:00:55 +0200

    Save krijgt data in database. nu zien da we meerdere blocks kunnen maken en saven. en da we de data ook terug in ons project krijgen

--- app/src/main/java/com/example/lyrics/ProjectActivity.java
+++ app/src/main/java/com/example/lyrics/ProjectActivity.java
@@ -8,20 +8,25 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProjectActivity extends AppCompatActivity {
 
@@ -30,6 +35,7 @@ public class ProjectActivity extends AppCompatActivity {
     String selectProjectURL = "https://studev.groept.be/api/a22pt108/selectProjectWithID/";
     Context context = this;
     Project project;
+    String saveURL = "https://studev.groept.be/api/a22pt108/UpdateBlocks";
 
 
     public void setProject(Project project) {
@@ -42,6 +48,7 @@ public class ProjectActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     int amntOfBlocks;
+    String section;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -122,10 +129,68 @@ public class ProjectActivity extends AppCompatActivity {
         requestQueue.add(queueRequest);
     }
 
+    private void requestSave(String Lyrics, String Types,int projectID){
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        StringRequest submitRequest = new StringRequest(
+                Request.Method.POST,
+                saveURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if( response.equals("[]")){
+
+                        }
+                        else {
+
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        ) { //NOTE THIS PART: here we are passing the POST parameters to the webservice
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("newblocktext", Lyrics);
+                params.put("newblocktypes", Types);
+                params.put("projectid", String.valueOf(projectID));
+                return params;
+            }
+        };
+
+        requestQueue.add(submitRequest);
+    }
+
     public void onBtnSaveClicked(View Caller){
+
+        // get Text From TextBlock
         TextInputEditText Lyrics = findViewById(R.id.LyricsTxt);
         String Text = String.valueOf(Lyrics.getText());
+        String modifiedText = Text.replace(" ","_")+";";
 
+        // get text from spinner
 
+        Spinner type = findViewById(R.id.SectionsSp);
+
+        type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                String Type = type.getItemAtPosition(position).toString();
+                section = Type+";";
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                section = "intro;";
+            }
+        });
+
+        requestSave(modifiedText,section,projectID);
     }
 }
