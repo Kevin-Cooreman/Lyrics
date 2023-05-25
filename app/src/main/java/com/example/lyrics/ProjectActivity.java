935814122e7291e440de0cbcac9078f47fe0239a
FLORIAN <florian.delplancke@student.kuleuven.be>
2023-05-25 21:50:17 +0200

    volledig nieuwe record en play

--- app/src/main/java/com/example/lyrics/ProjectActivity.java
+++ app/src/main/java/com/example/lyrics/ProjectActivity.java
@@ -9,6 +9,10 @@ import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContextWrapper;
+import android.media.MediaRecorder;
+
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
@@ -21,12 +25,14 @@ import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.FileUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.util.Base64;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -66,6 +72,7 @@ public class ProjectActivity extends AppCompatActivity {
     private static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT);
 
     private AudioRecord audioRecord;
+    private MediaRecorder mediaRecorder;
     private boolean isRecording = false;
     private byte[] audio;
     private MediaPlayer mediaPlayer;
@@ -102,7 +109,7 @@ public class ProjectActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.ProjectView);
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
-        if (checkMic()) {
+        if (checkForMic()) {
             getMicPermission();
         }
     }
@@ -207,27 +214,6 @@ public class ProjectActivity extends AppCompatActivity {
         requestQueue.add(submitRequest);
     }
 
-    private static void uploadAudio(byte[] audioData, int projectID) throws IOException {
-        URL url = new URL(uploadAudio_URL);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setDoOutput(true);
-        connection.setRequestProperty("Content-Type", "application/octet-stream");
-
-        // Set additional request parameters
-        String query = "projectid=" + URLEncoder.encode(String.valueOf(projectID), "UTF-8");
-
-        try (OutputStream outputStream = connection.getOutputStream()) {
-            outputStream.write(audioData);
-            outputStream.write("&".getBytes());
-            outputStream.write(query.getBytes());
-        }
-
-        int responseCode = connection.getResponseCode();
-        // Handle the response code or any other response as needed
-
-        connection.disconnect();
-    }
 
 
 
@@ -290,144 +276,42 @@ public class ProjectActivity extends AppCompatActivity {
 
 
     public void onBtnRecordPressed(View Caller) {
-        //vraag voor perrmission indien nodig
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            if (checkMic()) {
-                getMicPermission();
-            }
-            return;
+        try {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setOutputFile(getRecordingFilePath());
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+
+            Toast.makeText(this, "recording started", Toast.LENGTH_SHORT).show();
         }
-        audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
-        audioRecord.startRecording();
-        isRecording = true;
-
-        Thread recordingThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                byte[] buffer = new byte[BUFFER_SIZE];
-
-                try {
-                    while (isRecording) {
-                        int bytesRead = audioRecord.read(buffer, 0, BUFFER_SIZE);
-                        if (bytesRead != AudioRecord.ERROR_INVALID_OPERATION) {
-                            //Log.d("ProjectActivity", String.valueOf(buffer));
-                            outputStream.write(buffer, 0, bytesRead);
-                        }
-                    }
-                } finally {
-                    try {
-                        outputStream.close();
-                        byte[] audioData = outputStream.toByteArray();
-                        setAudio(audioData);
-                        String audioBase64 = getEncoder().encodeToString(audioData);
-                        uploadAudio(audioData,projectID);
-                        Log.d("ProjectActivity", "Audio byteArray: " + Arrays.toString(audioData));
-                        Log.d("ProjectActivity", "Audio base64: " + audioBase64);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-
-        recordingThread.start();
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
     public void onBtnStopPressed(View Caller){
-        isRecording = false;
-
-
-        if (audioRecord != null) {
-            audioRecord.stop();
-            audioRecord.release();
-            audioRecord = null;
-        }
-
+        mediaRecorder.stop();
+        mediaRecorder.reset();
+        mediaRecorder.release();
+        mediaRecorder = null;
+        Toast.makeText(this, "recording stopped", Toast.LENGTH_SHORT).show();
     }
-    public void onBtnPlayPressed(View Caller){
-
-        //byte[] wavArray = pcmToWav(audio,CHANNEL_CONFIG,SAMPLE_RATE,AUDIO_FORMAT);
-        //Log.d("ProjectActivity", "wav array: " + Arrays.toString(wavArray));
-        // Create a temporary audio file
-
-        //File tempAudioFile = File.createTempFile("temp_audio", ".wav", getDir());
-
-        // Write the byte array to the temporary audio file
-        //FileOutputStream fileOutputStream = new FileOutputStream(tempAudioFile);
-        //fileOutputStream.write(wavArray);
-        //fileOutputStream.close();
-        //Log.d("ProjectActivity", "length:" +  String.valueOf(tempAudioFile.length()));
-
-
-        Uri cacheFileUri = getCacheFileUri(context);
-        if (cacheFileUri != null) {
-            try {
-                MediaPlayer mediaPlayer = new MediaPlayer();
-                mediaPlayer.setDataSource(context, cacheFileUri);
-                mediaPlayer.prepare();
-                mediaPlayer.start();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("Failed to create cache file.");
-        }
-        /*
-            Uri myUri = Uri.fromFile(tempAudioFile); // initialize Uri here
-            MediaPlayer mediaPlayer = new MediaPlayer();
-            mediaPlayer.setAudioAttributes(
-                    new AudioAttributes.Builder()
-                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                            .setUsage(AudioAttributes.USAGE_MEDIA)
-                            .build()
-            );
-            mediaPlayer.setDataSource(getApplicationContext(), myUri);
+    public void onBtnPlayPressed(View Caller) {
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(getRecordingFilePath());
             mediaPlayer.prepare();
-
             mediaPlayer.start();
-            */
-
-/*
-            // Set the audio file as the data source for the MediaPlayer
-            Log.d("projectActivity", "sound file path: " + tempAudioFile.getAbsolutePath());
-
-            // Set the audio attributes for playback
-            AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                    .setUsage(AudioAttributes.USAGE_MEDIA)
-                    .build();
-            mediaPlayer.setAudioAttributes(audioAttributes);
-
-
-            // Set the onPreparedListener to start playback when prepared
-            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    mp.start();
-                }
-            });*/
-
-
-    }
-
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // Release the MediaPlayer resources
-        if (mediaPlayer != null) {
-            mediaPlayer.release();
-            mediaPlayer = null;
+            Toast.makeText(this, "recording is playing", Toast.LENGTH_SHORT).show();
+        }
+        catch( Exception e){
+            e.printStackTrace();
         }
     }
 
-    public void setAudio(byte[] audio) {
-        this.audio = audio;
-    }
-
-    private boolean checkMic(){
+    private boolean checkForMic(){
         if (this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE)){
             return true;
         }
@@ -439,111 +323,18 @@ public class ProjectActivity extends AppCompatActivity {
     private void getMicPermission(){
         if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO)
                 == PackageManager.PERMISSION_DENIED){
-
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.RECORD_AUDIO},
                     MICROPHONE_PERMISSION_CODE);
         }
     }
-
-    public static byte[] pcmToWav(byte[] pcmData, int numChannels, int sampleRate, int bitPerSample) {
-        byte[] wavData = new byte[pcmData.length + 44];
-        byte[] header = wavHeader(pcmData.length, numChannels, sampleRate, bitPerSample);
-        System.arraycopy(header, 0, wavData, 0, header.length);
-        System.arraycopy(pcmData, 0, wavData, header.length, pcmData.length);
-        return wavData;
+    private String getRecordingFilePath() throws IOException {
+        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
+        File musicDirectory = contextWrapper.getFilesDir();
+        File file = new File(musicDirectory, "testrecording" + ".mp3");
+        return file.getPath();
     }
 
-    /**
-     * @param pcmLen pcm数据长度
-     * @param numChannels 声道设置, mono = 1, stereo = 2
-     * @param sampleRate 采样频率
-     * @param bitPerSample 单次数据长度, 例如8bits
-     * @return wav头部信息
-     */
-    public static byte[] wavHeader(int pcmLen, int numChannels, int sampleRate, int bitPerSample) {
-        byte[] header = new byte[44];
-        // ChunkID, RIFF, 占4bytes
-        header[0] = 'R';
-        header[1] = 'I';
-        header[2] = 'F';
-        header[3] = 'F';
-        // ChunkSize, pcmLen + 36, 占4bytes
-        long chunkSize = pcmLen + 36;
-        header[4] = (byte) (chunkSize & 0xff);
-        header[5] = (byte) ((chunkSize >> 8) & 0xff);
-        header[6] = (byte) ((chunkSize >> 16) & 0xff);
-        header[7] = (byte) ((chunkSize >> 24) & 0xff);
-        // Format, WAVE, 占4bytes
-        header[8] = 'W';
-        header[9] = 'A';
-        header[10] = 'V';
-        header[11] = 'E';
-        // Subchunk1ID, 'fmt ', 占4bytes
-        header[12] = 'f';
-        header[13] = 'm';
-        header[14] = 't';
-        header[15] = ' ';
-        // Subchunk1Size, 16, 占4bytes
-        header[16] = 16;
-        header[17] = 0;
-        header[18] = 0;
-        header[19] = 0;
-        // AudioFormat, pcm = 1, 占2bytes
-        header[20] = 1;
-        header[21] = 0;
-        // NumChannels, mono = 1, stereo = 2, 占2bytes
-        header[22] = (byte) numChannels;
-        header[23] = 0;
-        // SampleRate, 占4bytes
-        header[24] = (byte) (sampleRate & 0xff);
-        header[25] = (byte) ((sampleRate >> 8) & 0xff);
-        header[26] = (byte) ((sampleRate >> 16) & 0xff);
-        header[27] = (byte) ((sampleRate >> 24) & 0xff);
-        // ByteRate = SampleRate * NumChannels * BitsPerSample / 8, 占4bytes
-        long byteRate = sampleRate * numChannels * bitPerSample / 8;
-        header[28] = (byte) (byteRate & 0xff);
-        header[29] = (byte) ((byteRate >> 8) & 0xff);
-        header[30] = (byte) ((byteRate >> 16) & 0xff);
-        header[31] = (byte) ((byteRate >> 24) & 0xff);
-        // BlockAlign = NumChannels * BitsPerSample / 8, 占2bytes
-        header[32] = (byte) (numChannels * bitPerSample / 8);
-        header[33] = 0;
-        // BitsPerSample, 占2bytes
-        header[34] = (byte) bitPerSample;
-        header[35] = 0;
-        // Subhunk2ID, data, 占4bytes
-        header[36] = 'd';
-        header[37] = 'a';
-        header[38] = 't';
-        header[39] = 'a';
-        // Subchunk2Size, 占4bytes
-        header[40] = (byte) (pcmLen & 0xff);
-        header[41] = (byte) ((pcmLen >> 8) & 0xff);
-        header[42] = (byte) ((pcmLen >> 16) & 0xff);
-        header[43] = (byte) ((pcmLen >> 24) & 0xff);
 
-        return header;
-    }
-
-    public Uri getCacheFileUri(Context context) {
-        File cacheFile = null;
-        try {
-            cacheFile = File.createTempFile("temp", ".wav", context.getCacheDir());
-            // Write the byte array to the temporary audio file
-            byte[] wavArray = pcmToWav(audio,CHANNEL_CONFIG,SAMPLE_RATE,AUDIO_FORMAT);
-            FileOutputStream fileOutputStream = new FileOutputStream(cacheFile);
-            fileOutputStream.write(wavArray);
-            fileOutputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if (cacheFile != null) {
-
-            return FileProvider.getUriForFile(context, "com.example.lyrics.fileprovider", cacheFile);
-        }
-        return null;
-    }
 
 
 }
